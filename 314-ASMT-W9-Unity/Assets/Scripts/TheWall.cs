91a14f8a6ca0f3b1626a039a2cec4501dfd9a31a
Grace Fowler <f.llfr.mgrace@gmail.com>
2025-04-04 22:30:52 -0400

    Spawned wall cube prefabs

--- 314-ASMT-W9-Unity/Assets/Scripts/TheWall.cs
+++ 314-ASMT-W9-Unity/Assets/Scripts/TheWall.cs
@@ -6,34 +6,65 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class TheWall : MonoBehaviour
 {
+    [SerializeField] GameObject wallCubePrefab;
+    [SerializeField] GameObject socketWallPrefab;
     [SerializeField] XRSocketInteractor wallSocket;
     [SerializeField] GameObject[] wallCubes;
+    [SerializeField] float cubeSpacing = 0.005f;
+    private Vector3 cubeSize;
+    private Vector3 spawnPosition;
     void Start()
     {
-        if(wallSocket != null)
+        if (wallCubePrefab != null)
         {
-            wallSocket.selectEntered.AddListener(OnSocketEnter);
-            wallSocket.selectExited.AddListener(OnSocketExited);
+            cubeSize = wallCubePrefab.GetComponent<Renderer>().bounds.size;
+        }
+
+        spawnPosition = transform.position;
+        BuildWall();
+    }
+    private void BuildWall()
+    {
+        wallCubes = new GameObject[2];
+        if (wallCubePrefab != null)
+        {
+            wallCubes[0] = Instantiate(wallCubePrefab, spawnPosition, transform.rotation);
+        }
+        spawnPosition.y += cubeSize.y + cubeSpacing;
+        if (socketWallPrefab != null)
+        {
+            wallCubes[1] = Instantiate(socketWallPrefab, spawnPosition, transform.rotation);
+            wallSocket = wallCubes[1].GetComponentInChildren<XRSocketInteractor>();
+            if (wallSocket != null)
+            {
+                wallSocket.selectEntered.AddListener(OnSocketEnter);
+                wallSocket.selectExited.AddListener(OnSocketExited);
+            }
+        }
+        for (int i = 0; i < wallCubes.Length; i++)
+        {
+            if (wallCubes[i] != null)
+            {
+                wallCubes[i].transform.SetParent(transform);
+            }
         }
     }
-
     private void OnSocketExited(SelectExitEventArgs arg0)
     {
         for (int i = 0; i < wallCubes.Length; i++)
         {
-            if(wallCubes[i] != null)
+            if (wallCubes[i] != null)
             {
                 Rigidbody rb = wallCubes[i].GetComponent<Rigidbody>();
                 rb.isKinematic = true;
             }
         }
     }
-
     private void OnSocketEnter(SelectEnterEventArgs arg0)
     {
         for (int i = 0; i < wallCubes.Length; i++)
         {
-            if(wallCubes[i] != null)
+            if (wallCubes[i] != null)
             {
                 Rigidbody rb = wallCubes[i].GetComponent<Rigidbody>();
                 rb.isKinematic = false;
@@ -44,6 +75,6 @@ public class TheWall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
